947015bd97e2982edb9a8ab739d56457f11ca7a3
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-29 14:46:41 -0300

    Más detalles sobre los objetos literales

--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
   nombre: 'Tony Stark',
   codeName: 'Ironman',
   vivo: false,
@@ -31,3 +31,26 @@ const x = 'vivo';
 console.log('Vivo', personaje[x]);
 
 console.log('Ulitima pelicula', personaje['ulitma-pelicula']);
+
+// Más detalles
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+Object.freeze(personaje);
+
+personaje.dinero = 100000000000;
+personaje.casado = false;
+personaje.direccion.ubicacion = 'Costa Rica';
+console.log(personaje);
+
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+console.log({propiedades});
+const valores = Object.values(personaje);
+console.log({ valores });
