f3ce8aa138abe6fe46f2514761bb0161cf5704b1
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-29 18:28:33 -0300

    While y Do while

---
+++ 01-fundamentos/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,35 @@
+
+
+const carros = ['Ford', 'Mazda', 'Honda', 'Toyota'];
+
+let i = 0;
+
+// while ( i < carros.length) {
+//   console.log( carros[i] );
+//   i++;
+// }
+
+// undefinded
+// null
+// false
+while (carros[i]) {
+  if ( i === 1) {
+    // break;
+    i++;
+    continue;
+  }
+  console.log( carros[i] );
+  i++;
+}
+
+
+
+console.warn('Do while');
+
+let j = 0;
+do {
+
+  console.log( carros[j] );
+  j++;
+
+} while (carros[j]);
--- 01-fundamentos/assets/js/palabras-reservadas.js
+++
@@ -1,8 +0,0 @@
-let objetos$ = 123;
-let precio99_99 = 123;
-
-let jugadorConPuntajeMasALto = 'Tobías';
-
-class JuegoAnio {
-  
-}
