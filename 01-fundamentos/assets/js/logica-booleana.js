e9bbb36fe3605166d4e479a68cfcc48683c7ae07
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-29 17:41:09 -0300

    Asignaciones con operadores

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -36,3 +36,21 @@ console.log(regresaTrue() || regresaFalse());
 console.log('4 condiciones ', true || true || true || false) // true
 
 console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && 'Hola mundo' && 150; // false
+const a2 = 'Hola' && 'Mundo' && soyFalso && true; // false
+const a3 = soyFalso || 'Ya no soy falso'; // 'Ya no soy falso'
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true; // 'Ya no soy falso de nuevo'
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true; // Regresa true
+
+console.log({a1, a2, a3, a4, a5});
+
+if (regresaFalse() && regresaTrue() && (true || false || true)) {
+  console.log('Hacer algo');
+} else {
+  console.log('Hacer otra cosa');
+}
