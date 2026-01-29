22db2a54ffaa50d345041921d8045613100a972c
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-29 14:39:05 -0300

    Objetos literales

---
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -0,0 +1,33 @@
+let personaje = {
+  nombre: 'Tony Stark',
+  codeName: 'Ironman',
+  vivo: false,
+  edad: 40,
+  coords: {
+    lat: 34.034,
+    lng: -118.70,
+  },
+  trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+  direccion: {
+    zip: '108880, 90265',
+    ubicacion: 'Malibu, California',
+  },
+  'ulitma-pelicula': 'Infinity War'
+};
+
+console.log(personaje);
+
+console.log('Nombre', personaje.nombre);
+console.log('Nombre', personaje['nombre']);
+console.log('Edad', personaje.edad);
+
+console.log('Coors', personaje.coords);
+console.log('Lat', personaje.coords.lat);
+
+console.log('No. Trajes', personaje.trajes.length);
+console.log('Ultimo traje', personaje.trajes[personaje.trajes.length - 1]);
+
+const x = 'vivo';
+console.log('Vivo', personaje[x]);
+
+console.log('Ulitima pelicula', personaje['ulitma-pelicula']);
