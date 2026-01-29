52a0362eb64ed54516408e8171a549133aca367a
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-29 16:54:57 -0300

    Alternativa if-else

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -25,3 +25,23 @@ if (dia === 0) {
 } else {
   console.log('No es domingo, lunes o martes');
 }
+
+// sin usar If else, o Swtich, unicamente objetos
+
+dia = 6;
+
+// día de la semana
+
+const diaLetras = {
+  0: 'Domingo',
+  1: 'Lunes',
+  2: 'Martes',
+  3: 'Miércoles',
+  4: 'Jueves',
+  5: 'Viernes',
+  6: 'Sábado',
+};
+
+const diaLetras2 = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+
+console.log(diaLetras2[dia]);
