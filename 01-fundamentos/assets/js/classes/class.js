a51156ab03b8d957881272ace0910acb3c1c583f
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-30 16:44:33 -0300

    Métodos en las clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -6,10 +6,18 @@ class Persona {
 
   constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase') {
 
-
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+  }
+
+  quienSoy() {
+    console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
+  }
+
+  miFrase() {
+    this.quienSoy();
+    console.log(`${this.codigo} dice: ${this.frase}`)
   }
 
 }
@@ -18,3 +26,6 @@ const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino 
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
 console.log(spiderman);
 console.log(ironman);
+
+spiderman.miFrase();
+ironman.miFrase();
