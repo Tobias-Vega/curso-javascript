7a9c1824c63dfe78f46f055534f249c81ba0a8d9
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-29 21:33:24 -0300

    Pedir carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+// Esta función crea una nueva baraja
 const crearDeck = () => {
 
   for (let i = 2; i <= 10; i++) {
@@ -22,9 +23,38 @@ const crearDeck = () => {
       deck.push(esp + tipo);
     }
   }
-  deck = _.shuffle(deck);
 
+  deck = _.shuffle(deck);
   return deck;
 }
 
 crearDeck();
+
+// Esta función me permite tomar una carta
+const pedirCarta = () => {
+
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+
+  const carta = deck.pop();
+
+  console.log(deck);
+  
+  console.log(carta) // carta debe de ser de la baraja
+
+  return carta;
+}
+
+pedirCarta();
+
+const valorCarta = (carta) => {
+
+  const valor = carta[0];
+
+  console.log({valor})
+
+
+}
+
+valorCarta('2D')
