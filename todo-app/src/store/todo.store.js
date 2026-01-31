14bc68e3c768880d1b8fed42038d241c0ddc528c
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-31 12:54:31 -0300

    Métodos que se ocuparán en el store

--- todo-app/src/store/todo.store.js
+++ todo-app/src/store/todo.store.js
@@ -1,25 +1,61 @@
-import { Todo } from "../todos/models/todo.model";
+import { Todo } from '../todos/models/todo.model';
 
 const Filters = {
   All: 'all',
   Completed: 'completed',
-  Pending: 'pending'
-}
+  Pending: 'pending',
+};
 
 const state = {
   todos: [
     new Todo('Piedra del alma'),
     new Todo('Piedra del infinito'),
-    new Todo('Piedra del tiempo')
+    new Todo('Piedra del tiempo'),
   ],
   filter: Filters.All,
-}
+};
 
 const initStore = () => {
   console.log(state);
   console.log('InitStore');
-}
+};
+
+const loadStore = () => {
+  throw new Error('Not implemented');
+};
+
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+};
+
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented');
+};
+
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented');
+};
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+};
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+};
 
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
   initStore,
-}
+  loadStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
+};
