915d5989c16e36c70071bd048f345bddf12f1285
Tobias Vega <tobiasvega1210@gmail.com>
2026-01-30 21:10:36 -0300

    Estructura y estilos de la aplicación

---
+++ todo-app/src/todos/app.js
@@ -0,0 +1,19 @@
+import html from './app.html?raw';
+
+/**
+ * 
+ * @param {string} elementId
+ * @returns 
+ */
+
+export const App = (elementId) => {
+
+  // Cuando la funcion App() se llama
+  (() => {
+    const app = document.createElement('div');
+    app.innerHTML = html;
+    document.querySelector(elementId).append(app);
+  })();
+
+
+}
