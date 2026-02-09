1775f68cb20d485e1b97528e7210ca7ee70e7fae
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-09 19:52:42 -0300

    funciones generadoras

--- 05-advanced/src/main.js
+++ 05-advanced/src/main.js
@@ -7,6 +7,7 @@ import { asyncComponent } from './concepts/05-async';
 import { asyncAwaitComponent } from './concepts/06-async-await';
 import { asyncAwait2Component } from './concepts/07-async-await';
 import { forAwaitComponent } from './concepts/08-for-await';
+import { generationFunctionComponent } from './concepts/09-generators';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -32,4 +33,5 @@ const element = document.querySelector('.card');
 // asyncComponent(element);
 // asyncAwaitComponent(element);
 // asyncAwait2Component(element);
-forAwaitComponent(element);
+// forAwaitComponent(element);
+generationFunctionComponent(element);
