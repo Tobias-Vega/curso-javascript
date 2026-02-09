e821a50ced8bb90b4ed35bff811061b095cbbeba
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-09 17:06:39 -0300

    Promise hell

--- 05-advanced/src/concepts/03-promises.js
+++ 05-advanced/src/concepts/03-promises.js
@@ -9,17 +9,30 @@ export const promiseComponent = (element) => {
     element.innerHTML = hero.name;
   };
 
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+      <h3>${hero1.name}</h3>
+      <h3>${hero2.name}</h3>
+    `;
+  };
+
   const renderError = (error) => {
     element.innerHTML = `
       <h1>Error:</h1>
       <h3>${error}</h3>
     `;
-  }
+  };
   const id1 = '5d86371fd55e2e2a30fe1ccb2';
   const id2 = '5d86371fd55e2e2a30fe1cc3';
 
   findeHero(id1)
-    .then(renderHero)
+    .then((hero1) => {
+      findeHero(id2)
+        .then((hero2) => {
+          renderTwoHeroes(hero1, hero2);
+        })
+        .catch(renderError);
+    })
     .catch(renderError);
 };
 
