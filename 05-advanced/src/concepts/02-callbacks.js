936ea59fc4459be404a072e6a3238816d28e6f0c
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-09 13:43:40 -0300

    Callbacks y manejo de errores

---
+++ 05-advanced/src/concepts/02-callbacks.js
@@ -0,0 +1,37 @@
+import { heroes } from '../data/heroes';
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const callbacksComponent = (element) => {
+  const id = '5d86371f233c9f2425f16916';
+
+  findHero(id, (error, hero) => {
+    // element.innerHTML = hero?.name || 'No hay heroe';
+
+    if (error) {
+      element.innerHTML = error;
+      return;
+    }
+
+    element.innerHTML = hero.name;
+  });
+};
+
+/**
+ *
+ * @param {String} id
+ * @param {(error: String|null, hero: Object) => void} callback
+ */
+
+const findHero = (id, callback) => {
+  const hero = heroes.find((hero) => hero.id === id);
+
+  if (!hero) {
+    callback(`Hero with ${id} not found`);
+    return; // undefinded
+  }
+
+  callback(null, hero);
+};
