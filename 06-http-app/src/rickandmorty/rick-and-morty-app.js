3d65ba9f762a9bb7f548e92d8f47f7710377937f
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-10 13:32:10 -0300

    Fetch get

--- 06-http-app/src/rickandmorty/rick-and-morty-app.js
+++ 06-http-app/src/rickandmorty/rick-and-morty-app.js
@@ -1,8 +1,30 @@
+
+
+/**
+ * @returns {Promise<Object>} Character information
+ */
+const fetchCharacter = async () => {
+
+  const res = await fetch('https://rickandmortyapi.com/api/character');
+
+  const { results } = await res.json();
+
+  console.log(results);
+  
+  return results;
+
+  
+}
+
 /**
  *
  * @param {HTMLDivElement} element
  */
-export const RickAndMortyApp = (element) => {
+export const RickAndMortyApp = async (element) => {
   document.querySelector('#app-title').innerHTML = 'RickAndMortyApp App';
   element.innerHTML = 'Loading...';
+
+  const character = await fetchCharacter();
+
+  element.innerHTML = 'Tenemos data!!';
 };
