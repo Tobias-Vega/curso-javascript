0c691dba0ce0792ff7ce69a6a9a838fc7714cdaf
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-10 14:05:19 -0300

    Fin sección 12 - Next Character

--- 06-http-app/src/rickandmorty/rick-and-morty-app.js
+++ 06-http-app/src/rickandmorty/rick-and-morty-app.js
@@ -1,20 +1,15 @@
-
-
 /**
  * @returns {Promise<Object>} Character information
  */
 const fetchCharacter = async () => {
-
   const res = await fetch('https://rickandmortyapi.com/api/character');
 
   const { results } = await res.json();
 
   console.log(results);
-  
-  return results;
 
-  
-}
+  return results;
+};
 
 /**
  *
@@ -24,7 +19,35 @@ export const RickAndMortyApp = async (element) => {
   document.querySelector('#app-title').innerHTML = 'RickAndMortyApp App';
   element.innerHTML = 'Loading...';
 
-  const character = await fetchCharacter();
+  const characterLabel = document.createElement('h3');
+  const statusLabel = document.createElement('h3');
+  const specieLabel = document.createElement('h3');
+  const nextCharacterButton = document.createElement('button');
+  nextCharacterButton.innerText = 'Next Character';
 
-  element.innerHTML = 'Tenemos data!!';
+  let index = 0;
+  const data = await fetchCharacter();
+
+  const renderCharacter = () => {
+    characterLabel.innerText = `Nombre: ${data[index].name}`;
+    statusLabel.innerText = `Estado: ${data[index].status}`;
+    specieLabel.innerText = `Especie: ${data[index].species}`;
+
+    element.replaceChildren(
+      characterLabel,
+      statusLabel,
+      specieLabel,
+      nextCharacterButton,
+    );
+  };
+
+  nextCharacterButton.addEventListener('click', () => {
+    index++;
+
+    if (index >= data.length) index = 0;
+
+    renderCharacter();
+  });
+
+  renderCharacter();
 };
