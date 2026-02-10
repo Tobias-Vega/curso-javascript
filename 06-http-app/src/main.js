49e7d2b8b8c59d56acc7ec53192f9d5ce01785e6
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-09 23:00:28 -0300

    Inicio Sección 12 - Peticiones HTTP

---
+++ 06-http-app/src/main.js
@@ -0,0 +1,23 @@
+import './style.css';
+import viteLogo from '../public/vite.svg'
+import javascriptLogo from './javascript.svg';
+import { RickAndMortyApp } from './rickandmorty/rick-and-morty-app';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1 id="app-title">Hello Vite!</h1>
+    <div class="card">
+    
+    </div>
+  </div>
+`;
+
+const element = document.querySelector('.card');
+
+RickAndMortyApp(element);
---
+++ 06-http-app/src/rickandmorty/rick-and-morty-app.js
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const RickAndMortyApp = (element) => {
+  document.querySelector('#app-title').innerHTML = 'RickAndMortyApp App';
+  element.innerHTML = 'Loading...';
+};
