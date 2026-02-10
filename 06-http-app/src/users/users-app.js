798781f815761f867dc94816817d1113bcbba70e
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-10 16:41:20 -0300

    Botones prev, next y current agregados

--- 06-http-app/src/users/users-app.js
+++ 06-http-app/src/users/users-app.js
@@ -1,20 +1,17 @@
-import { renderTable } from "./presentation/render-table/render-table";
-import usersStore from "./store/users-store";
-
+import { renderButtons } from './presentation/render-buttons/render-button';
+import { renderTable } from './presentation/render-table/render-table';
+import usersStore from './store/users-store';
 
 /**
  * @param {HTMLDivElement} element
  */
 export const UserApp = async (element) => {
-
   element.innerHTML = 'Loading...';
 
   await usersStore.loadNextPage();
 
   element.innerHTML = '';
 
-  renderTable(element)
-
-
-
-}
+  renderTable(element);
+  renderButtons(element);
+};
