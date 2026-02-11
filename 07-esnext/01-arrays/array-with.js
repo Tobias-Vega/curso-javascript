38300c43d0ef5f2ca5e450410d3a54038cbcb418
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-11 11:17:05 -0300

    Inicio Sección 14 - Creación del proyecto

---
+++ 07-esnext/01-arrays/array-with.js
@@ -0,0 +1,18 @@
+const superHeroes = [
+  {
+    id: 1,
+    name: 'Batman',
+  },
+  {
+    id: 2,
+    name: 'Superman',
+  },
+  {
+    id: 3,
+    name: 'Flash',
+  },
+  {
+    id: 4,
+    name: 'Aquaman',
+  },
+]
